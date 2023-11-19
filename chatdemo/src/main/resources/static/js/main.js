4dcae081cd8562c6519259fb159101b5adc2eed8
kidy1998 <zzangkidy@naver.com>
2023-11-19 22:55:43 +0900

    1119

--- chatdemo/src/main/resources/static/js/main.js
+++ chatdemo/src/main/resources/static/js/main.js
@@ -1,5 +1,6 @@
 var stompClient = null;
 
+
 function setConnected(connected) {
     $("#connect").prop("disabled", connected);
     $("#disconnect").prop("disabled", !connected);
@@ -13,7 +14,12 @@ function setConnected(connected) {
     $("#msg").html("");
 }
 
+
 function connect() {
+
+    const mainContent = document.getElementById('main-content');
+    mainContent.style.visibility = 'visible';
+    document.getElementById('msg').focus();
     var socket = new SockJS('/ws');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
@@ -22,7 +28,6 @@ function connect() {
         stompClient.send("/app/sendMessage", {}, JSON.stringify("welcome")); //서버에 보낼 메시지
         stompClient.subscribe('/topic/public', function (message) {
             showMessage("받은 메시지: " + message.body); //서버에 메시지 전달 후 리턴받는 메시지
-            console.log("message : ", message);
         });
     });
 }
@@ -34,6 +39,8 @@ function disconnect() {
     setConnected(false);
     console.log("Disconnected");
     $("#communicate").empty(); // 자식 요소 제거
+    const mainContent = document.getElementById('main-content');
+    mainContent.style.visibility = 'hidden';
 }
 
 function sendMessage() {
@@ -52,7 +59,32 @@ $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();     //form 태그의 페이지 이동 기능을 막음
     });
-    $( "#connect" ).click(function() { connect(); });  // 웹소켓 연결 버튼 누르면 실행
-    $( "#disconnect" ).click(function() { disconnect(); }); // 웹소켓 연결해제 버튼 누르면 실행
+
+    $("#connect").click(function () {
+        connect();
+    });
+
+    $("#disconnect").click(function () {
+        disconnect(); 
+    });
+    
     $( "#send" ).click(function() { sendMessage(); });   // 보내기 버튼 누르면 실행
 });
+
+
+/*
+document.getElementById('connect').addEventListener('click', function () {
+    // 챗봇 이미지 클릭 시 실행되는 함수
+    document.getElementById('main-content').style.display = 'block';
+});
+
+document.getElementById('disconnect').addEventListener('click', function () {
+    // 챗봇 이미지 클릭 시 실행되는 함수
+    document.getElementById('main-content').style.display = 'none';
+});
+*/
+
+
+// const disconnectButton = document.getElementById('disconnect');
+
+
